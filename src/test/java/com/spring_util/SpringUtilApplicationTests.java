6bd0856a8b9fb73da1fde384c3628afdd6b9461b
jiaohongtao <jiaohongtao@beyondcent.com>
2020-03-13 16:20:18 +0800

    初始数据

---
+++ src/test/java/com/spring_util/SpringUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
