d0991e2c327e8ba3c4afc04c4a8989f5032048e2
jiaohongtao <jiaohongtao@beyondcent.com>
2020-03-17 14:54:04 +0800

    代码反射生成单个表的实体类

---
+++ src/main/java/com/spring_util/Mysql2Entity.java
@@ -0,0 +1,269 @@
+package com.spring_util;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 单个生成数据库表实体类
+ * https://blog.csdn.net/wyazyf/article/details/104531044/
+ */
+public class Mysql2Entity {
+
+    //指定实体生成所在包的路径
+    private static String basePath = new File("").getAbsolutePath();
+    private String packageOutPath = "com.spring_util";//指定实体生成所在包的路径
+    private String authorName = "jht";//作者名字
+    private static final SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private String[] colnames; // 列名数组
+    private String[] colTypes; //列名类型数组
+    private int[] colSizes; //列名大小数组
+    private boolean f_util = false; // 是否需要导入包java.util.*
+    private boolean f_sql = false; // 是否需要导入包java.sql.*
+
+    private String tableName = "cmdb_module";//表名
+    //数据库连接
+    private static final String URL = "jdbc:mysql://10.20.10.16:3306/plus_cmdb";
+    private static final String NAME = "root";
+    private static final String PASS = "onceas";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    /**
+     * 功能：获取并创建实体所在的路径目录
+     *
+     * @return
+     */
+    private String pkgDirName() {
+        String outputPath = basePath + "/src/main/java/" + this.packageOutPath.replace(".", "/");
+        File dir = new File(outputPath);
+        if (!dir.exists()) {
+            dir.mkdirs();
+            System.out.println("mkdirs dir 【" + outputPath + "】");
+        }
+        return outputPath;
+    }
+
+    /*
+     * 构造函数
+     */
+    public Mysql2Entity() {
+        //创建连接
+        Connection con;
+        //查要生成实体类的表
+        String sql = "select * from " + tableName;
+
+        tableName = initcap1(tableName);
+        PreparedStatement pStemt;
+        try {
+            try {
+                Class.forName(DRIVER);
+            } catch (ClassNotFoundException e1) {
+                e1.printStackTrace();
+            }
+            con = DriverManager.getConnection(URL, NAME, PASS);
+            pStemt = con.prepareStatement(sql);
+            ResultSetMetaData rsmd = pStemt.getMetaData();
+            int size = rsmd.getColumnCount();    //统计列
+            colnames = new String[size];
+            colTypes = new String[size];
+            colSizes = new int[size];
+            for (int i = 0; i < size; i++) {
+                colnames[i] = initcap1(rsmd.getColumnName(i + 1));
+                colTypes[i] = rsmd.getColumnTypeName(i + 1);
+
+                if (colTypes[i].equalsIgnoreCase("datetime")) {
+                    f_util = true;
+                }
+                if (colTypes[i].equalsIgnoreCase("image") || colTypes[i].equalsIgnoreCase("text")) {
+                    f_sql = true;
+                }
+                colSizes[i] = rsmd.getColumnDisplaySize(i + 1);
+            }
+
+            String content = parse(colnames, colTypes, colSizes);
+
+            try {
+                File directory = new File("");
+                // String path = this.getClass().getResource("").getPath();
+                String outputPath = directory.getAbsolutePath() + "/src/main/java/" + this.packageOutPath.replace(".", "/") + "/" + initcap(tableName) + ".java";
+                FileWriter fw = new FileWriter(outputPath);
+                PrintWriter pw = new PrintWriter(fw);
+                pw.println(content);
+                pw.flush();
+                pw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+//            try {
+//                con.close();
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+
+    /**
+     * 功能：生成实体类主体代码
+     *
+     * @param colNames
+     * @param colTypes
+     * @param colSizes
+     * @return
+     */
+    private String parse(String[] colNames, String[] colTypes, int[] colSizes) {
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("package " + this.packageOutPath + ";\r\n");
+        //判断是否导入工具包
+        if (f_util) {
+            sb.append("import java.util.Date;\r\n");
+        }
+        if (f_sql) {
+            sb.append("import java.sql.*;\r\n");
+        }
+        sb.append("\r\n");
+
+        // 注释部分
+        sb.append("/**\r\n");
+        sb.append(" * table name:  " + tableName + "\r\n");
+        sb.append(" * author name: " + authorName + "\r\n");
+        sb.append(" * create time: " + SDF.format(new Date()) + "\r\n");
+        sb.append(" */\r\n");
+        //实体部分
+        sb.append("public class " + initcap(tableName) + "{\r\n");
+        processAllAttrs(sb);//属性
+        processAllMethod(sb);//get set方法
+        sb.append("}\r\n");
+
+        //System.out.println(sb.toString());
+        return sb.toString();
+    }
+
+    /**
+     * 功能：生成所有属性
+     *
+     * @param sb
+     */
+    private void processAllAttrs(StringBuffer sb) {
+
+        for (int i = 0; i < colnames.length; i++) {
+            sb.append("\tprivate " + sqlType2JavaType(colTypes[i]) + " " + colnames[i] + ";\r\n");
+        }
+
+    }
+
+    /**
+     * 功能：生成所有方法
+     *
+     * @param sb
+     */
+    private void processAllMethod(StringBuffer sb) {
+
+        for (int i = 0; i < colnames.length; i++) {
+            sb.append("\tpublic void set" + initcap(colnames[i]) + "(" + sqlType2JavaType(colTypes[i]) + " " +
+                    colnames[i] + "){\r\n");
+            sb.append("\tthis." + colnames[i] + "=" + colnames[i] + ";\r\n");
+            sb.append("\t}\r\n");
+            sb.append("\tpublic " + sqlType2JavaType(colTypes[i]) + " get" + initcap(colnames[i]) + "(){\r\n");
+            sb.append("\t\treturn " + colnames[i] + ";\r\n");
+            sb.append("\t}\r\n");
+        }
+
+    }
+
+    /**
+     * 功能：将输入字符串的首字母改成大写
+     *
+     * @param str
+     * @return
+     */
+    private String initcap(String str) {
+
+        char[] ch = str.toCharArray();
+        if (ch[0] >= 'a' && ch[0] <= 'z') {
+            ch[0] = (char) (ch[0] - 32);
+        }
+
+        return new String(ch);
+    }
+
+    /**
+     * 功能：将输入字符串的蛇形命名改成用大写字母隔开
+     *
+     * @param str1 str1
+     * @return
+     */
+    private String initcap1(String str1) {
+
+        String[] str1s = str1.split("_");
+
+        String feild = str1s[0];
+        for (int i = 1; i < str1s.length; i++) {
+            char[] ch = str1s[i].toCharArray();
+            if (ch[0] >= 'a' && ch[0] <= 'z') {
+                ch[0] = (char) (ch[0] - 32);
+            }
+            feild += new String(ch);
+        }
+
+
+        return feild;
+    }
+
+    /**
+     * 功能：获得列的数据类型
+     *
+     * @param sqlType
+     * @return
+     */
+    private String sqlType2JavaType(String sqlType) {
+
+        if (sqlType.equalsIgnoreCase("bit")) {
+            return "boolean";
+        } else if (sqlType.equalsIgnoreCase("tinyint")) {
+            return "byte";
+        } else if (sqlType.equalsIgnoreCase("smallint")) {
+            return "short";
+        } else if (sqlType.equalsIgnoreCase("int")) {
+            return "int";
+        } else if (sqlType.equalsIgnoreCase("bigint")) {
+            return "long";
+        } else if (sqlType.equalsIgnoreCase("float")) {
+            return "float";
+        } else if (sqlType.equalsIgnoreCase("decimal") || sqlType.equalsIgnoreCase("numeric")
+                || sqlType.equalsIgnoreCase("real") || sqlType.equalsIgnoreCase("money")
+                || sqlType.equalsIgnoreCase("smallmoney")) {
+            return "double";
+        } else if (sqlType.equalsIgnoreCase("varchar") || sqlType.equalsIgnoreCase("char")
+                || sqlType.equalsIgnoreCase("nvarchar") || sqlType.equalsIgnoreCase("nchar")
+                || sqlType.equalsIgnoreCase("text")) {
+            return "String";
+        } else if (sqlType.equalsIgnoreCase("datetime")) {
+            return "Date";
+        } else if (sqlType.equalsIgnoreCase("image")) {
+            return "Blod";
+        }
+
+        return null;
+    }
+
+    /**
+     * 出口
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        new Mysql2Entity();
+
+    }
+
+}
