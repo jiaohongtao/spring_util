09a014d14ca42f1fe34201fd74864d1d7cdf71a4
jiaoht <18332120276@163.com>
2020-07-07 22:47:56 +0800

    for无限循环

---
+++ src/main/java/com/spring_util/test/test_20200707/UtilLoop.java
@@ -0,0 +1,30 @@
+package com.spring_util.test.test_20200707;
+
+import java.util.Date;
+
+/**
+ * for无限循环
+ *
+ * @author jiaohongtao
+ * @version 1.0
+ * @since 2020/7/7 22:40
+ */
+public class UtilLoop {
+
+    public static void main(String[] args) {
+
+        for (; ; ) {
+            System.out.println(new Date());
+            System.out.println("我执行了");
+            System.out.println(Thread.currentThread().getName());
+            Thread.currentThread().setName("MAIN");
+            System.out.println("重写当前线程名称为：" + Thread.currentThread().getName());
+            System.out.println("===我睡5秒====");
+            try {
+                Thread.sleep(5 * 1000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
