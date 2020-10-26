4bf609440c8f646e959b58f8bac6a135bcc11670
jiaohongtao <jiaohongtao@beyondcent.com>
2020-10-26 16:22:06 +0800

    测试回退

---
+++ src/main/java/com/springutil/test/DeleteTest.java
@@ -0,0 +1,13 @@
+package com.springutil.test;
+
+/**
+ * @author jiaohongtao
+ * @version 1.0
+ * @since 2020年10月26日
+ */
+public class DeleteTest {
+
+    public static void main(String[] args) {
+        System.out.println("abcd");
+    }
+}
